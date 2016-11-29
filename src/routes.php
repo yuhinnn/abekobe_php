27388d0432b4e4818af4378a7ae4371a4afce1b1
yuhi_deve <yuhinnn@gmail.com>
2016-11-29 15:51:03 +0900

    add config

--- src/routes.php
+++ src/routes.php
@@ -8,3 +8,7 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/about', function () {
+
+});
